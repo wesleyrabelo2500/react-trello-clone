9b359df421b0c148a1f2fb27877e000dde7a8a0e
Wodorek <58212786+Wodorek@users.noreply.github.com>
2021-09-09 15:18:51 +0200

    fixed firebase import
    Firebase needs to be imported from /compat folder in v9

--- src/auth/utils/AuthHOC.js
+++ src/auth/utils/AuthHOC.js
@@ -1,4 +1,5 @@
-import firebase from 'firebase';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { SIGN_IN } from '../../routes';
