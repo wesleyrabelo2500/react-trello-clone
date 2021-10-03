6b307d5c3774ee2b830f0e681b3cf8024c4d411f
jayeshchoudhary <jayeshchoudhary619@gmail.com>
2021-10-03 18:40:09 +0530

    Fix #136: Add BoardLink.js prop types

--- src/features/boards/components/BoardLink.js
+++ src/features/boards/components/BoardLink.js
@@ -1,11 +1,18 @@
+import PropTypes from 'prop-types'; // ES6
 import { Icon } from 'antd';
 import { Favorite, StyledBoardLink, Title } from '../styles';
 
-export const BoardLink = ({ title, favorite }) => (
-    <StyledBoardLink color="#0079BF">
+export const BoardLink = ({ title, favorite, color }) => (
+    <StyledBoardLink color={color || "#0079BF"}>
         <Title>{title}</Title>
         <Favorite favorite={favorite}>
             <Icon type="star" />
         </Favorite>
     </StyledBoardLink>
 );
+
+BoardLink.propTypes = {
+    title: PropTypes.string.isRequired,
+    favorite: PropTypes.string,
+    color: PropTypes.string
+}
