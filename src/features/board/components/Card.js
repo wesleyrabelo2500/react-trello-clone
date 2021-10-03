558b9433e0eef1fd3b3d5b7e941e2cbdd1b21ed2
Harsh <harshrai456@gmail.com>
2021-10-03 19:39:31 +0530

    Refactored Card Component to functional Component from Class Component

--- src/features/board/components/Card.js
+++ src/features/board/components/Card.js
@@ -1,74 +1,71 @@
 import { Icon } from 'antd';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { LABELS } from '../../../core/constants';
 import { GrayButton } from '../../../shared/components/Button';
 import { CardBlock, Edit, TitleInput } from '../styles';
 import { Label } from './common/Label';
 
-export class Card extends Component {
-    state = {
+export function Card(props) {
+    const [state, setState] = useState({
         showEditIcons: false,
         editMode: false,
         title: '',
-    };
+    });
 
-    handleSubmitForm = async (event, callback, listKey, cardKey, title) => {
+    const handleSubmitForm = async (event, callback, listKey, cardKey, title) => {
         event.preventDefault();
         await callback(listKey, cardKey, { title });
-        this.setState(() => ({
+        setState((prvsState) => ({
+            ...prvsState,
             editMode: false,
         }));
     };
 
-    getColor(labels, text) {
-        return labels.find((label) => label.text === text).color;
-    }
+    const getColor = (labels, text) => {
+        const label = labels.find((label) => label.text === text);
+        return label.color;
+    };
 
-    render() {
-        const { showModal, onEditCard, onDeleteCard, card, listKey } = this.props;
-        const { showEditIcons, editMode, title } = this.state;
-        return (
-            <CardBlock
-                onMouseEnter={() => this.setState(() => ({ showEditIcons: true }))}
-                onMouseLeave={() => this.setState(() => ({ showEditIcons: false }))}
-                onBlur={() => this.setState(() => ({ editMode: false }))}
-                editMode={editMode}
-                onClick={showModal}
-            >
-                <div>
-                    {card && card.label && (
-                        <Label
-                            color={this.getColor(LABELS, card.label)}
-                            text={card.label}
-                            small={true}
-                        />
-                    )}
-                </div>
-                {editMode ? (
-                    <form
-                        onSubmit={(event) =>
-                            this.handleSubmitForm(event, onEditCard, listKey, card.key, title)
-                        }
-                    >
-                        <TitleInput
-                            value={this.state.title}
-                            onChange={(event) => this.setState({ title: event.target.value })}
-                        />
-                    </form>
-                ) : (
-                    <React.Fragment>
-                        {showEditIcons && (
-                            <Edit onClick={(event) => event.stopPropagation()}>
-                                <GrayButton onClick={() => onDeleteCard(listKey, card.key)}>
-                                    <Icon type="delete" />
-                                </GrayButton>
-                            </Edit>
-                        )}
-                        <div>{card.title}</div>
-                    </React.Fragment>
+    const { showModal, onEditCard, onDeleteCard, card, listKey } = props;
+    const { showEditIcons, editMode, title } = state;
+
+    return (
+        <CardBlock
+            onMouseEnter={() => setState((prvsState) => ({ ...prvsState, showEditIcons: true }))}
+            onMouseLeave={() => setState((prvsState) => ({ ...prvsState, showEditIcons: false }))}
+            onBlur={() => setState((prvsState) => ({ ...prvsState, editMode: false }))}
+            editMode={editMode}
+            onClick={showModal}
+        >
+            <div>
+                {card && card.label && (
+                    <Label color={getColor(LABELS, card.label)} text={card.label} small={true} />
                 )}
-                <div>{card.description && <Icon type="align-left" />}</div>
-            </CardBlock>
-        );
-    }
+            </div>
+            {editMode ? (
+                <form
+                    onSubmit={(event) =>
+                        handleSubmitForm(event, onEditCard, listKey, card.key, title)
+                    }
+                >
+                    <TitleInput
+                        value={state.title}
+                        onChange={(event) => setState({ title: event.target.value })}
+                    />
+                </form>
+            ) : (
+                <React.Fragment>
+                    {showEditIcons && (
+                        <Edit onClick={(event) => event.stopPropagation()}>
+                            <GrayButton onClick={() => onDeleteCard(listKey, card.key)}>
+                                <Icon type="delete" />
+                            </GrayButton>
+                        </Edit>
+                    )}
+                    <div>{card.title}</div>
+                </React.Fragment>
+            )}
+            <div>{card.description && <Icon type="align-left" />}</div>
+        </CardBlock>
+    );
 }
