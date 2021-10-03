76bdbe1600e076ee64a65e911a9a204b18c8795b
Bhavin789 <bhavib.789@gmail.com>
2021-10-03 12:52:23 +0530

    refactor(Account): convert account component to functional

--- src/auth/pages/Account.js
+++ src/auth/pages/Account.js
@@ -1,5 +1,5 @@
 import { Form, Icon, Input } from 'antd';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import { passwordUpdate } from '../api/auth';
 import { byPropKey } from '../../shared/utils';
@@ -15,95 +15,95 @@ const INITIAL_STATE = {
     error: null,
 };
 
-class AccountScreen extends Component {
-    state = { ...INITIAL_STATE };
+const AccountScreen = (props) => {
 
-    handleSubmit = (event) => {
+    const [state, setState] = useState(INITIAL_STATE)
+
+    const handleSubmit = (event) => {
         event.preventDefault();
 
-        return passwordUpdate(this.state.passwordOne)
+        return passwordUpdate(state.passwordOne)
             .then(() => {
-                this.props.form.setFieldsValue({
+                props.form.setFieldsValue({
                     passwordOne: '',
                     passwordTwo: '',
                 });
             })
             .catch((error) => {
-                this.setState(byPropKey('error', error.message));
+                setState(byPropKey('error', error.message));
             });
     };
 
-    render() {
-        const { getFieldDecorator } = this.props.form;
-        const { error } = this.state;
-        return (
-            <AuthUserContext.Consumer>
-                {(authUser) => (
-                    <FormContainer>
-                        <h3>Account: {authUser.email}</h3>
 
-                        <Form onSubmit={(event) => this.handleSubmit(event)} className="login-form">
-                            <Form.Item>
-                                {getFieldDecorator('passwordOne', {
-                                    rules: [
-                                        { required: true, message: 'Please input your Password!' },
-                                    ],
-                                })(
-                                    <Input
-                                        prefix={
-                                            <Icon
-                                                type="lock"
-                                                style={{ color: 'rgba(0,0,0,.25)' }}
-                                            />
-                                        }
-                                        onChange={(event) =>
-                                            this.setState(
-                                                byPropKey('passwordOne', event.target.value)
-                                            )
-                                        }
-                                        type="password"
-                                        placeholder="Password"
-                                    />
-                                )}
-                            </Form.Item>
+    const { getFieldDecorator } = props.form;
+    const { error } = state;
+    return (
+        <AuthUserContext.Consumer>
+            {(authUser) => (
+                <FormContainer>
+                    <h3>Account: {authUser.email}</h3>
 
-                            <Form.Item>
-                                {getFieldDecorator('passwordTwo', {
-                                    rules: [
-                                        { required: true, message: 'Please input your Password!' },
-                                    ],
-                                })(
-                                    <Input
-                                        prefix={
-                                            <Icon
-                                                type="lock"
-                                                style={{ color: 'rgba(0,0,0,.25)' }}
-                                            />
-                                        }
-                                        onChange={(event) =>
-                                            this.setState(
-                                                byPropKey('passwordTwo', event.target.value)
-                                            )
-                                        }
-                                        type="password"
-                                        placeholder="Password"
-                                    />
-                                )}
-                            </Form.Item>
+                    <Form onSubmit={(event) => handleSubmit(event)} className="login-form">
+                        <Form.Item>
+                            {getFieldDecorator('passwordOne', {
+                                rules: [
+                                    { required: true, message: 'Please input your Password!' },
+                                ],
+                            })(
+                                <Input
+                                    prefix={
+                                        <Icon
+                                            type="lock"
+                                            style={{ color: 'rgba(0,0,0,.25)' }}
+                                        />
+                                    }
+                                    onChange={(event) =>
+                                        setState(
+                                            byPropKey('passwordOne', event.target.value)
+                                        )
+                                    }
+                                    type="password"
+                                    placeholder="Password"
+                                />
+                            )}
+                        </Form.Item>
 
-                            <Form.Item>
-                                <FormButton type="primary" htmlType="submit">
-                                    Reset my password
-                                </FormButton>
-                            </Form.Item>
+                        <Form.Item>
+                            {getFieldDecorator('passwordTwo', {
+                                rules: [
+                                    { required: true, message: 'Please input your Password!' },
+                                ],
+                            })(
+                                <Input
+                                    prefix={
+                                        <Icon
+                                            type="lock"
+                                            style={{ color: 'rgba(0,0,0,.25)' }}
+                                        />
+                                    }
+                                    onChange={(event) =>
+                                        setState(
+                                            byPropKey('passwordTwo', event.target.value)
+                                        )
+                                    }
+                                    type="password"
+                                    placeholder="Password"
+                                />
+                            )}
+                        </Form.Item>
 
-                            <ErrorMessage>{error}</ErrorMessage>
-                        </Form>
-                    </FormContainer>
-                )}
-            </AuthUserContext.Consumer>
-        );
-    }
+                        <Form.Item>
+                            <FormButton type="primary" htmlType="submit">
+                                Reset my password
+                            </FormButton>
+                        </Form.Item>
+
+                        <ErrorMessage>{error}</ErrorMessage>
+                    </Form>
+                </FormContainer>
+            )}
+        </AuthUserContext.Consumer>
+    );
 }
 
 export default withAuthorization((authUser) => !!authUser)(Form.create()(AccountScreen));
