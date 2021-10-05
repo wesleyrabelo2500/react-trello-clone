38516f1c4812ca41e226dd240145a988f785ab4d
Sander Nobel <sander.nobel@protonmail.com>
2021-10-05 18:13:15 +0200

    Upgraded react-dnd to newest version. Changed Context to Provider in line with documentation

--- src/auth/pages/SignUp.js
+++ src/auth/pages/SignUp.js
@@ -1,5 +1,5 @@
 import { Form, Input } from 'antd';
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import { createUser } from '../../core/services/db';
 import { LANDING } from '../../routes';
