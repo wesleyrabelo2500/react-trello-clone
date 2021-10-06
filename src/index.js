3c7bd825a67bcac1cd5fb5c92074e0622a4b7f07
Dmitry Moiseev <mdmitry94@gmail.com>
2021-10-06 23:01:39 +0300

    move reducer to utils

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import reducer, { initialState } from './services/reducer';
-import { StateProvider } from './services/StateProvider';
+import reducer, { initialState } from './utils/reducer';
+import { StateProvider } from './utils/state-provider';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
