180d4ce3c4df0adfa2f383c53872763656f8271d
Soufiane Elharem <soufiane.mar9@gmail.com>
2021-10-10 18:47:17 +0100

    Add 404 page

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,3 +1,28 @@
 import React from 'react';
 
-export const NotFoundPage = () => <h1>404</h1>;
+export const NotFoundPage = () => (
+    <>
+        <img
+            alt="404"
+            wifth
+            src="Images/404.svg"
+            style={{
+                display: 'block',
+                width: '60%',
+                height: '70%',
+                margin: 'auto',
+                marginTop: '5%',
+            }}
+        />
+        <p
+            style={{
+                textAlign: 'center',
+                marginTop: '2%',
+                fontFamil: 'Time new Roman',
+                fontSize: '25px',
+            }}
+        >
+            The page isn't available. Sorry!
+        </p>
+    </>
+);
