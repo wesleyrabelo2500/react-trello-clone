72f511a09f1b72af9f18b392eacdc988818d9bad
Soufiane Elharem <soufiane.mar9@gmail.com>
2021-10-11 22:33:38 +0100

    Added 404Page with css classes

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,28 +1,9 @@
 import React from 'react';
+import './NotFoundPage.css';
 
 export const NotFoundPage = () => (
     <>
-        <img
-            alt="404"
-            wifth
-            src="Images/404.svg"
-            style={{
-                display: 'block',
-                width: '60%',
-                height: '70%',
-                margin: 'auto',
-                marginTop: '5%',
-            }}
-        />
-        <p
-            style={{
-                textAlign: 'center',
-                marginTop: '2%',
-                fontFamil: 'Time new Roman',
-                fontSize: '25px',
-            }}
-        >
-            The page isn't available. Sorry!
-        </p>
+        <img alt="404" src="Images/404.svg" className="notFoundImage" />
+        <p className="notFoundText">The page isn't available. Sorry!</p>
     </>
 );
