27bba16897e0f249477e1369f3868835b70d3303
Roopa <roopavidhyaa@gmail.com>
2021-10-12 21:34:33 +0530

    Add Skeleton for Board page

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { BoardTitle } from './BoardTitle';
 export { BoardModal } from './BoardModal';
 export { Navbar } from './Navbar';
 export { BoardsPageSkeleton } from './BoardsPageSkeleton';
+export { BoardSkeleton } from './BoardSkeleton';
