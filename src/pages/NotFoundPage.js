e7f173adf5c9487a9f239f909dd7ca843e2a4d13
Soufiane Elharem <soufiane.mar9@gmail.com>
2021-10-12 16:48:50 +0100

    Added 404Page with tailwindcss

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './NotFoundPage.css';
+//import './NotFoundPage.css';
 
 export const NotFoundPage = () => (
     <>
-        <img alt="404" src="Images/404.svg" className="notFoundImage" />
-        <p className="notFoundText">The page isn't available. Sorry!</p>
+        <img alt="404" src="Images/404.svg" className="m-auto my-10 h-4/5 w-3/5 block" />
+        <p className="text-center mt-2 text-2xl">The page isn't available. Sorry!</p>
     </>
 );
